a2f174bc870750d7d8a64191e18b8f09771d7cdd
devel0per <devel0perzek001@gmail.com>
2025-09-09 11:36:21 +0300

    Implement AI content generation feature with Google GenAI integration

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,29 @@
-const express = require("express");
-const app = express()
-const PORT = process.env.PORT || 3000
+require("dotenv").config();
 
-app.get('/', (req, res) => {
-    res.send("Menged API is running successfully")
-})
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const { GoogleGenAI } = require("@google/genai");
+
+const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Menged API is running successfully");
+});
+
+app.post("/generate", async (req, res) => {
+    const { message } = await req.body;
+    const response = await ai.models.generateContent({
+        model: "gemini-2.5-flash",
+        contents: message,
+    });
+    console.log(response.text);
+
+    res.json({ reply: response.text });
+});
 
 app.listen(PORT, () => {
-    console.log(`Server is running ${PORT}`)
-})
+    console.log(`Server is running ${PORT}`);
+});
