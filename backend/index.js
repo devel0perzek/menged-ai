61818068cd6fe02e483453f4dc90f6a50bd26b7a
Eyuale <eyualekifle99@gmail.com>
2025-09-13 09:30:35 +0300

    Add authentication middleware to lesson routes and update package dependencies

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 // IMPORTS
 const express = require("express");
 const { GoogleGenAI } = require("@google/genai");
+const { clerkMiddleware, requireAuth } = require("@clerk/express")
 const lessonRoutes = require("./routes/lessons.routes")
 
 // CONFIGURATIONS
@@ -10,8 +11,9 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const ai = new GoogleGenAI({ apiKey: process.env.GEMINI_API_KEY });
 app.use(express.json());
+app.use(clerkMiddleware())
 
-app.use('/lessons', lessonRoutes)
+app.use('/lessons', requireAuth(),lessonRoutes)
 
 app.get("/", (req, res) => {
     res.send("Menged API is running successfully");
